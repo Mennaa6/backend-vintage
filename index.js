4abda930e25cefc0ff614030feb4540e24d65a7f
Mennaa6 <mennaabdelfattahhh@gmail.com>
2025-05-08 20:05:04 +0300

    initial setup

---
+++ index.js
@@ -0,0 +1,19 @@
+const express = require("express");
+const cors = require("cors");
+
+const app = express();
+const PORT = 5000;
+
+app.use(cors());
+app.use(express.json());
+
+// Test route
+app.get("/api/test", (req, res) => {
+  res.json({ message: "Hello World" });
+});
+
+// Start server
+// app.listen(PORT, () => {
+//   console.log(`Server is running on http://localhost:${PORT}`);
+// });
+module.exports = app;
